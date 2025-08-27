7e50faab4ba5d952709da14fc7813040d7fcba50
Tomas-ETL <to.estorres11@gmail.com>
2025-08-26 21:52:41 -0400

    Add files via upload

---
+++ proyecto-Front/src/app/models/usuario.ts
@@ -0,0 +1,11 @@
+export interface Usuario {
+  nombre: string;
+  email: string;
+  password: string;
+  edad: number;
+  genero: string;
+  objetivo: string[];
+  condicion: string[];
+  altura: number;
+  peso: number;
+}
